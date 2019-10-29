a616e5a28d3e004b35d02c65c266e10bb0555baf
Joe Bateson <joe@joebateson.com>
2019-10-28 22:35:54 -0700

    feat(deps): upgrade deps and remove unnecessary react hooks rules

--- index.js
+++ index.js
@@ -1,9 +1,6 @@
 module.exports = {
   extends: ['react-app', 'plugin:prettier/recommended', 'prettier/react'],
-  plugins: ['react-hooks'],
   rules: {
     'import/order': ['warn', { 'newlines-between': 'always' }],
-    'react-hooks/rules-of-hooks': 'error',
-    'react-hooks/exhaustive-deps': 'warn',
   },
 };
