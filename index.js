10f42c5a956fa101a36a16c709841dabc51db279
Joe Bateson <joe@joebateson.com>
2019-12-24 19:23:25 +0000

    feat(rule): enable prefer-const as warning

--- index.js
+++ index.js
@@ -2,5 +2,6 @@ module.exports = {
   extends: ['react-app', 'plugin:prettier/recommended', 'prettier/react'],
   rules: {
     'import/order': ['warn', { 'newlines-between': 'always' }],
+    'prefer-const': 'warn',
   },
 };
