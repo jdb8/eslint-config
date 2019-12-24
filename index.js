9404a4d736d5c26867fc1270c16d48a89607b107
Joe Bateson <joe@joebateson.com>
2019-12-24 19:43:49 +0000

    feat(rule): enable no-var as warning

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ module.exports = {
   extends: ['react-app', 'plugin:prettier/recommended', 'prettier/react'],
   rules: {
     'import/order': ['warn', { 'newlines-between': 'always' }],
+    'no-var': 'warn',
     'prefer-const': 'warn',
   },
 };
